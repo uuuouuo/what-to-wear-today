ef1f2af658245e9faaa51140d677c7a17edc196a
16010948 <bjyoung0@gmail.com>
2022-01-27 11:45:56 +0900

    [feat] kakao login redirect page 구현

---
+++ frontend/src/pages/callback/kakao/styled.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  height: 100vh;
+  padding: 5vh;
+  filter: drop-shadow(0px 20px 10px rgba(0, 0, 0, 0.15));
+  background-image: linear-gradient(135deg, #ad6bff 0%, #74bcf7 100%);
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default { Container };
