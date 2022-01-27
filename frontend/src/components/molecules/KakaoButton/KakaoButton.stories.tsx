979ddb0cc490b3f7c6421eb4821b5c240737c3f9
16010948 <bjyoung0@gmail.com>
2022-01-27 11:42:44 +0900

    [feat] KakaoButton 컴포넌트 구현
    - KakaoButton 컴포넌트 구현
    - 테스트 코드 작성

---
+++ frontend/src/components/molecules/KakaoButton/KakaoButton.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import KakaoButton from './KakaoButton';
+
+export default {
+  title: 'molecules/KakaoButton',
+  component: KakaoButton,
+};
+
+export const Default = () => {
+  return <KakaoButton />;
+};
