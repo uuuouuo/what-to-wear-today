a0ef1c412bc5ad5cd736c9a0f5ff50dfb98417c5
16010948 <bjyoung0@gmail.com>
2022-01-30 07:35:11 +0900

    [refactor] atoms 컴포넌트 공통 export 추가
    - 다른 컴포넌트에서 import할 때 atom을 한 번에 import할 수 있게 하기 위해 공통 export 추가

---
+++ frontend/src/components/atoms/index.ts
@@ -0,0 +1,11 @@
+import Button from './Button/Button';
+import CheckBox from './CheckBox/CheckBox';
+import FileInput from './FileInput/FileInput';
+import FooterContainer from './FooterContainer/FooterContainer';
+import Heading from './Heading/Heading';
+import Input from './Input/Input';
+import Label from './Label/Label';
+import MainContainer from './MainContainer/MainContainer';
+import Text from './Text/Text';
+
+export { Button, CheckBox, FileInput, FooterContainer, Heading, Input, Label, MainContainer, Text };
