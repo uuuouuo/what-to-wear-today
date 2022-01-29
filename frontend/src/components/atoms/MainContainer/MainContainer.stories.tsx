03a9802a94bfc53019eefc6584d184e4093c9df6
16010948 <bjyoung0@gmail.com>
2022-01-30 07:33:12 +0900

    [feat] MainContainer Component 구현
    - 배경이 되는 컨테이너 컴포넌트로 구현

---
+++ frontend/src/components/atoms/MainContainer/MainContainer.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import MainContainer from './MainContainer';
+
+export default {
+  title: 'Atoms/MainContainer',
+  component: MainContainer,
+};
+
+export const Default = () => {
+  return <MainContainer />;
+};
