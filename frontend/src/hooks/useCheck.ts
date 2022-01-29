7eaa4a6c9dd7b7309a96e0eed4507711530e48d9
16010948 <bjyoung0@gmail.com>
2022-01-30 07:44:11 +0900

    [feat] useCheck hook 구현
    - CheckBox에서 값을 선택할 때, 제거할 때 사용할 수 있는 훅 구현

---
+++ frontend/src/hooks/useCheck.ts
@@ -0,0 +1,24 @@
+import React, { useCallback, useState } from 'react';
+
+const useCheck = <T extends { checked: boolean; value: string }>(
+  initalState: string[],
+): [
+  string[],
+  React.Dispatch<React.SetStateAction<string[]>>,
+  (e: React.ChangeEvent<T>) => void,
+] => {
+  const [values, setValues] = useState(initalState);
+
+  const onChange = useCallback(
+    (e: React.ChangeEvent<T>) => {
+      if (e.target.checked) setValues([...values, e.target.value]);
+      else setValues(values.filter((value) => value !== e.target.value));
+      console.log(e.target.value);
+    },
+    [values],
+  );
+
+  return [values, setValues, onChange];
+};
+
+export default useCheck;
