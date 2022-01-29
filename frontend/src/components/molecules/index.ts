02bd82576c76537866c39e0a589794208eea348b
16010948 <bjyoung0@gmail.com>
2022-01-30 07:43:10 +0900

    [refactor] molecule 공통 export 생성
    - molecule을 import할 때 한 줄로 import 할 수 있도록 공통 export 생성

---
+++ frontend/src/components/molecules/index.ts
@@ -0,0 +1,9 @@
+import CheckBoxFormGroup from './CheckBoxFormGroup/CheckBoxFormGroup';
+import FormGroup from './FormGroup/FormGroup';
+import FormGroupWithIcon from './FormGroupWithIcon/FormGroupWithIcon';
+import ImageUpload from './ImageUpload/ImageUpload';
+import KakaoButton from './KakaoButton/KakaoButton';
+import Modal from './Modal/Modal';
+import Title from './Title/Title';
+
+export { CheckBoxFormGroup, FormGroup, FormGroupWithIcon, ImageUpload, KakaoButton, Modal, Title };
