a395201ec080a30693c9ea98122a190c8787d5b1
16010948 <bjyoung0@gmail.com>
2022-02-02 16:32:39 +0900

    [feat] 구현된 molecule export 추가 / 수정

--- frontend/src/components/molecules/index.ts
+++ frontend/src/components/molecules/index.ts
@@ -1,9 +1,19 @@
-import CheckBoxFormGroup from './CheckBoxFormGroup/CheckBoxFormGroup';
+import CheckInputFormGroup from './CheckInputFormGroup/CheckInputFormGroup';
 import FormGroup from './FormGroup/FormGroup';
 import FormGroupWithIcon from './FormGroupWithIcon/FormGroupWithIcon';
 import ImageUpload from './ImageUpload/ImageUpload';
 import KakaoButton from './KakaoButton/KakaoButton';
 import Modal from './Modal/Modal';
+import SuccessBadge from './SuccessBadge/SuccessBadge';
 import Title from './Title/Title';
 
-export { CheckBoxFormGroup, FormGroup, FormGroupWithIcon, ImageUpload, KakaoButton, Modal, Title };
+export {
+  CheckInputFormGroup,
+  FormGroup,
+  FormGroupWithIcon,
+  ImageUpload,
+  KakaoButton,
+  Modal,
+  SuccessBadge,
+  Title,
+};
