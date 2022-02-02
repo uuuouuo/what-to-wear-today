79570dc552c4212cb0692cdd3d9153df5d063bc7
dohyun <dodohyun0807@gmail.com>
2022-02-02 22:56:15 +0900

    [docs] 헤더 부분 스타일 이동

---
+++ frontend/src/components/Header/Header.styled.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const Header = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  height: 5vh;
+  padding: 0 1vw 0 1vw;
+  background-color: orange;
+`;
+
+export default { Header };
