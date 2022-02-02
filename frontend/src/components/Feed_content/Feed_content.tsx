3b47aae9f1d9de02f40895599b66be122ce3e2db
dohyun <dodohyun0807@gmail.com>
2022-02-02 14:21:39 +0900

    [feat] 인터섹션 옵저버로 인피니티 스크롤 구현 인터섹션 옵저버 api를 이용해 피드 콘텐츠 부분 인피니티 스크롤 구현

--- frontend/src/components/Feed_content/Feed_content.tsx
+++ frontend/src/components/Feed_content/Feed_content.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 // material UI icon
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
@@ -11,15 +11,77 @@ import Styled from './Feed_content.styled';
 import Button from '../atoms/Button/Button';
 
 const Feed_article = () => {
-  const Follow = () => {
-    console.log('follow!');
+  const scrollAreaRef = useRef<HTMLDivElement>(null);
+
+  const target = useRef(null);
+
+  const loadItems = () => {
+    setData((prevState) => {
+      const articles = [
+        { id: 11, name: '홍' },
+        { id: 12, name: 'ㅂ' },
+        { id: 13, name: 'ㅈ' },
+        { id: 14, name: 'ㄷ' },
+        { id: 15, name: 'ㄱ' },
+        { id: 16, name: 'ㅅ' },
+        { id: 17, name: 'ㅛ' },
+        { id: 18, name: 'ㅕ' },
+        { id: 19, name: 'ㅑ' },
+        { id: 20, name: 'ㅐ' },
+        { id: 21, name: 'ㅔ' },
+        { id: 22, name: 'ㅁ' },
+        { id: 23, name: 'ㄴ' },
+        { id: 24, name: 'ㅇ' },
+        { id: 25, name: 'ㄹ' },
+        { id: 26, name: 'ㅎ' },
+        { id: 27, name: 'ㅗ' },
+        { id: 28, name: 'ㅓ' },
+        { id: 29, name: 'ㅏ' },
+        { id: 30, name: 'ㅣ' },
+        { id: 31, name: ';' },
+        { id: 32, name: 'ㅋ' },
+      ];
+      const id = prevState[prevState.length - 1].id;
+      const articleId = articles.map((article, index) => {
+        return { ...article, id: id + index + 1 };
+      });
+      return [...prevState, ...articleId];
+    });
   };
+
+  useEffect(() => {
+    const options = {
+      root: scrollAreaRef.current,
+      threshold: 0.4,
+    };
+
+    const onIntersect: IntersectionObserverCallback = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+          return;
+        }
+        loadItems();
+        observer.unobserve(entry.target);
+        observer.observe(target.current);
+      });
+    };
+
+    const io = new IntersectionObserver(onIntersect, options);
+
+    if (target.current) {
+      io.observe(target.current);
+    }
+
+    return () => io && io.disconnect();
+  }, [scrollAreaRef, target]);
+
   interface sampleType {
     id: number;
     name: string;
   }
 
-  const sample: sampleType[] = [
+  const [datas, setData] = useState([
+    { id: 0, name: '김이박' },
     { id: 1, name: '김' },
     { id: 2, name: '이' },
     { id: 3, name: '박' },
@@ -29,37 +91,45 @@ const Feed_article = () => {
     { id: 7, name: '길' },
     { id: 8, name: '선우' },
     { id: 9, name: '독고' },
-    { id: 10, name: '홍' },
-  ];
+  ]);
+
+  //////////////////////////////////////
+
+  const Follow = () => {
+    console.log('follow!');
+  };
 
   return (
-    <Styled.Feed_content_box>
-      {sample.map((idx) => (
-        <Styled.Feed_content>
-          <Styled.Feed_content_top>
-            <div>사진{idx.name}</div>
-            <div>
+    <Styled.Feed_content_box ref={scrollAreaRef}>
+      {datas.map((article, idx) => {
+        const lastEl = idx === datas.length - 1;
+        return (
+          <Styled.Feed_content key={idx} ref={lastEl ? target : null}>
+            <Styled.Feed_content_top>
+              <div>사진{article.name}</div>
               <div>
-                <Styled.Feed_content_user>
-                  <div>유저네임{idx.name}</div>
-                  <div>아이디{idx.name}</div>
-                  <div>게시글 올린시간(ago){idx.name}</div>
-                </Styled.Feed_content_user>
-                <div>게시글 내용{idx.name}</div>
-                <div>해시태그{idx.name}</div>
+                <div>
+                  <Styled.Feed_content_user>
+                    <div>유저네임{article.name}</div>
+                    <div>아이디{article.name}</div>
+                    <div>게시글 올린시간(ago){article.name}</div>
+                  </Styled.Feed_content_user>
+                  <div>게시글 내용{article.name}</div>
+                  <div>해시태그{article.name}</div>
+                </div>
               </div>
-            </div>
-            <div>
-              {/* 팔로우 버튼 */}
-              <Button children={<p>Follow</p>} onClick={Follow}></Button>
-            </div>
-          </Styled.Feed_content_top>
-          <Styled.Feed_content_bottom>
-            <ChatBubbleOutlineIcon />
-            <FavoriteBorderIcon />
-          </Styled.Feed_content_bottom>
-        </Styled.Feed_content>
-      ))}
+              <div>
+                {/* 팔로우 버튼 */}
+                <Button children={<p>Follow</p>} onClick={Follow}></Button>
+              </div>
+            </Styled.Feed_content_top>
+            <Styled.Feed_content_bottom>
+              <ChatBubbleOutlineIcon />
+              <FavoriteBorderIcon />
+            </Styled.Feed_content_bottom>
+          </Styled.Feed_content>
+        );
+      })}
     </Styled.Feed_content_box>
   );
 };
