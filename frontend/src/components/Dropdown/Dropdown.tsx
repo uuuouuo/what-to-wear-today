d765b8f89c76244808f96e4b7cb3742caeacd3ba
dohyun <dodohyun0807@gmail.com>
2022-02-03 23:52:33 +0900

    [docs]dropdown 구현 방식 변경

--- frontend/src/components/Dropdown/Dropdown.tsx
+++ frontend/src/components/Dropdown/Dropdown.tsx
@@ -1,12 +1,18 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
+import React from 'react';
+import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Link from 'next/link';
 
-export default function Dropdown() {
+const options = ['수정', '삭제', '공유'];
+
+const ITEM_HEIGHT = 35;
+
+const Dropdown = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
@@ -15,28 +21,40 @@ export default function Dropdown() {
 
   return (
     <div>
-      <Button
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
+      <IconButton
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? 'long-menu' : undefined}
         aria-expanded={open ? 'true' : undefined}
+        aria-haspopup="true"
         onClick={handleClick}
       >
-        Dashboard
-      </Button>
+        <MoreHorizIcon />
+      </IconButton>
       <Menu
-        id="basic-menu"
+        id="long-menu"
+        MenuListProps={{
+          'aria-labelledby': 'long-button',
+        }}
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: '20vw',
+          },
         }}
       >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        {options.map((option) => (
+          <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
+            {/* 각 페이지 링크 설정 값 부여 해야 함 */}
+            <Link href={'/feed'}>{option}</Link>
+          </MenuItem>
+        ))}
       </Menu>
     </div>
   );
-}
+};
+
+export default Dropdown;
