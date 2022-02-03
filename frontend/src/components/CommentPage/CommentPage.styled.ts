8705efe3ae3d1cafdc413e33f62b840ede7e62e2
dohyun <dodohyun0807@gmail.com>
2022-02-03 23:53:12 +0900

    [docs]댓글 영역 초기 구현 및 스타일 작업

--- frontend/src/components/CommentPage/CommentPage.styled.ts
+++ frontend/src/components/CommentPage/CommentPage.styled.ts
@@ -1,1 +1,13 @@
 import styled from '@emotion/styled';
+
+const CommentPageLayout = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+`;
+
+const WriteArea = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+`;
+
+export default { CommentPageLayout, WriteArea };
--- frontend/src/components/CommentPage/CommentPage.tsx
+++ frontend/src/components/CommentPage/CommentPage.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-// import Styled from './CommentPage.styled';
+import Styled from './CommentPage.styled';
 
 import Button from 'components/atoms/Button/Button';
 import Input from 'components/atoms/Input/Input';
+import UserImage from '../atoms/UserImage/UserImage';
 
 const action = () => {
   console.log('action에 넣을 함수');
@@ -10,22 +11,23 @@ const action = () => {
 
 const CommentPage = () => {
   return (
-    <>
+    <Styled.CommentPageLayout>
       {/* 댓글 입력 영역 */}
-      <div>
-        <div>프로필사진영역</div>
+      <Styled.WriteArea>
+        <UserImage userId={1} />
         <Input placeholder="댓글 달기..." value="" onChange={action} />
         <Button children="Reply" type="submit" onClick={action} />
-      </div>
+      </Styled.WriteArea>
       {/* 댓글리스트 영역 */}
       <div>
-        <div>프로필사진영역</div>
+        <UserImage userId={1} />
         <div>이름</div>
         <div>댓글내용</div>
         <div>날짜시각</div>
+        {/* 슬라이드로 구현해볼까? */}
         <div>본인에게만 보이는 수정 삭제 아이콘</div>
       </div>
-    </>
+    </Styled.CommentPageLayout>
   );
 };
 
