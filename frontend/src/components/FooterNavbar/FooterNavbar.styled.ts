ee267dbe001b488fc298fb0ab3e53963ed104da0
dohyun <dodohyun0807@gmail.com>
2022-02-03 23:51:58 +0900

    [docs]푸터 네비게이션바 materialUI로 교체

--- frontend/src/components/FooterNavbar/FooterNavbar.styled.ts
+++ frontend/src/components/FooterNavbar/FooterNavbar.styled.ts
@@ -1,12 +1,6 @@
 import styled from '@emotion/styled';
 
 const FooterNavbar = styled.div`
-  position: fixed;
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-between;
-  align-items: center;
-  bottom: 0;
   width: 100%;
   height: 5vh;
 `;
--- frontend/src/components/FooterNavbar/FooterNavbar.tsx
+++ frontend/src/components/FooterNavbar/FooterNavbar.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
-import Link from 'next/link';
-
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import PersonIcon from '@mui/icons-material/Person';
 import Styled from './FooterNavbar.styled';
 
-const FooterBar = () => {
+const FooterNavbar = () => {
+  const [value, setValue] = React.useState(0);
+
   return (
     <Styled.FooterNavbar>
-      {/* 홈 */}
-      <Link href={'/'}>홈</Link>
-      {/* 상세 검색 */}
-      <Link href={'/'}>검색</Link>
-      {/* 게시글 작성 */}
-      <Link href={'/'}>게시글 작성</Link>
-      {/* 좋아요피드 */}
-      <Link href={'/'}>좋아요피드</Link>
-      {/* 마이페이지 */}
-      <Link href={'/'}>마이페이지</Link>
+      <Box>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction icon={<HomeIcon />} />
+          <BottomNavigationAction icon={<SearchIcon />} />
+          <BottomNavigationAction icon={<AddCircleIcon />} />
+          <BottomNavigationAction icon={<FavoriteIcon />} />
+          <BottomNavigationAction icon={<PersonIcon />} />
+        </BottomNavigation>
+      </Box>
     </Styled.FooterNavbar>
   );
 };
 
-export default FooterBar;
+export default FooterNavbar;
