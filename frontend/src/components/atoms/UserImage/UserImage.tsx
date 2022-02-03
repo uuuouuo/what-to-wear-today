7b9464ae6e1f73b54452e454519cd787b274ecba
dohyun <dodohyun0807@gmail.com>
2022-02-03 23:54:56 +0900

    [docs]프로필 사진 materilUI로 교체

--- frontend/src/components/atoms/UserImage/UserImage.tsx
+++ frontend/src/components/atoms/UserImage/UserImage.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react';
+import Avatar from '@mui/material/Avatar';
 
 interface Props {
   userId: number;
@@ -6,12 +7,7 @@ interface Props {
 }
 
 const UserImage: FunctionComponent<Props> = ({ userId, className }) => {
-  return (
-    <div>
-      {/* {userId.Img} */}
-      사진
-    </div>
-  );
+  return <Avatar alt="dobby" src={'/images/profileIMG/sample.jpg'} />;
 };
 
 export default UserImage;
