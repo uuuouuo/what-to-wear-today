e9f9e0f360b0934f1f99d7d76f1a107e72a18015
강민철 <kmc9308@naver.com>
2022-02-04 13:35:09 +0900

    [feat] 회원정보 페이지 추가 코멘트 탭 추가 피드탭의 날짜 input 추가

--- frontend/src/pages/user/[index].tsx
+++ frontend/src/pages/user/[index].tsx
@@ -4,12 +4,20 @@ import Image from 'next/image';
 
 import Styled from './styled';
 import { Title } from '@/components/molecules';
-import { Label, Button, Text } from '@/components/atoms';
+import { Label, Button, Input } from '@/components/atoms';
 import Avatar from '@mui/material/Avatar';
 import SettingsIcon from '@mui/icons-material/Settings';
+import SearchIcon from '@mui/icons-material/Search';
+
+import { useRouter } from 'next/router';
 
 const User: NextPage = () => {
+  const router = useRouter();
+  const { index } = router.query;
+
   const [activeIndex, setActiveIndex] = useState(0);
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
   const dummyFeed = [
     { title: '더미 피드 1', content: '헤헿' },
     { title: '더미 피드 2', content: '헤헿' },
@@ -24,6 +32,13 @@ const User: NextPage = () => {
     { title: '더미 라이크 4', content: '헤헿' },
     { title: '더미 라이크 5', content: '헤헿' },
   ];
+  const dummyComment = [
+    { title: '더미 코멘트 1', content: '헤헿' },
+    { title: '더미 코멘트 2', content: '헤헿' },
+    { title: '더미 코멘트 3', content: '헤헿' },
+    { title: '더미 코멘트 4', content: '헤헿' },
+    { title: '더미 코멘트 5', content: '헤헿' },
+  ];
   const content = [
     {
       tab: 'feed',
@@ -35,13 +50,27 @@ const User: NextPage = () => {
       id: 1,
       content: dummyLike,
     },
+    {
+      tab: 'comment',
+      id: 2,
+      content: dummyComment,
+    },
   ];
 
-  const modifyProfile = () => {};
-  const switchTab = (event: React.MouseEvent<HTMLElement>) => {
-    // console.log(event);
-    const idx = event.target.value;
-    setActiveIndex(idx);
+  const modifyProfile = () => {
+    console.log(index);
+  };
+  const switchTab = (e: React.MouseEvent<HTMLElement>) => {
+    // console.log(e.target.value);
+    const idx = e.target.value;
+    setActiveIndex(Number(idx));
+  };
+  const searchFeed = () => {
+    console.log('search');
+  };
+  const tmp = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.valueAsDate);
+    console.log(event.target.valueAsNumber);
   };
   return (
     <Styled.MainContainer>
@@ -56,23 +85,36 @@ const User: NextPage = () => {
             </Button>
           </Styled.rowContainer>
           <Styled.rowContainer>
+            <Label>더위를 잘 타는 타입</Label>
+          </Styled.rowContainer>
+          <Styled.rowContainer>
             <Label>팔로잉 : </Label>
             <Label>팔로워 : </Label>
           </Styled.rowContainer>
         </Styled.columnContainer>
       </Styled.ProfileContainer>
       <Styled.rowContainer>
-        {content.map((section, index) => {
+        {content.map((section, idx) => {
           return (
-            <button key={index} value={index} onClick={switchTab}>
+            <button key={idx} value={idx} onClick={switchTab}>
               {section.tab}
             </button>
           );
         })}
       </Styled.rowContainer>
       <Styled.contentContainer>
-        {content[activeIndex].content.map((item, index) => {
-          return <Styled.columnContainer key={index}>{item.title}</Styled.columnContainer>;
+        {activeIndex === 0 ? (
+          <Styled.rowContainer>
+            기간 <input type="date" onChange={tmp}></input> ~ <input type="date"></input>
+            <Button onClick={searchFeed}>
+              <SearchIcon />
+            </Button>
+          </Styled.rowContainer>
+        ) : null}
+      </Styled.contentContainer>
+      <Styled.contentContainer>
+        {content[activeIndex].content.map((item, idx) => {
+          return <Styled.columnContainer key={idx}>{item.title}</Styled.columnContainer>;
         })}
       </Styled.contentContainer>
     </Styled.MainContainer>
