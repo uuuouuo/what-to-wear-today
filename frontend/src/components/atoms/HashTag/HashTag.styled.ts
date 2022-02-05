5313e3a2f5f571cb734abfb745c412230c3ddc0f
dohyun <dodohyun0807@gmail.com>
2022-02-05 23:23:11 +0900

    [docs]해시태그 레이아웃 및 컬러 변경

--- frontend/src/components/atoms/HashTag/HashTag.styled.ts
+++ frontend/src/components/atoms/HashTag/HashTag.styled.ts
@@ -3,6 +3,8 @@ import styled from '@emotion/styled';
 const HashTag = styled.div`
   display: flex;
   flex-flow: row nowrap;
+  color: skyblue;
+  margin-bottom: 1vh;
 `;
 
 export default { HashTag };
