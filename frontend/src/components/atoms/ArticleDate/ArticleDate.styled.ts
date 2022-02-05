2dc9f7212d7c8d29ff2280ca07cf616c427cb435
dohyun <dodohyun0807@gmail.com>
2022-02-05 23:22:34 +0900

    [docs]Date 정보 스타일 적용을 위한 수정

--- frontend/src/components/atoms/ArticleDate/ArticleDate.styled.ts
+++ frontend/src/components/atoms/ArticleDate/ArticleDate.styled.ts
@@ -1,1 +1,9 @@
 import styled from '@emotion/styled';
+
+const ArticleDateLayout = styled.div`
+  font-size: 12px;
+  color: silver;
+  margin-bottom: 1vh;
+`;
+
+export default { ArticleDateLayout };
