f3ee99d4c71cf56f4767c903df8dcb73c85937ba
dohyun <dodohyun0807@gmail.com>
2022-02-06 23:06:43 +0900

    [feat]알림창에서 스와이프로 삭제하는 기능 생성 react-swipeable-list추가, 인피니티스크롤 불필요로 삭제

--- frontend/src/components/NotificationContent/NotificationContent.tsx
+++ frontend/src/components/NotificationContent/NotificationContent.tsx
@@ -1,84 +1,22 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { SwipeableList, SwipeableListItem } from 'react-swipeable-list';
+import React, { useState } from 'react';
+import {
+  SwipeableList,
+  SwipeableListItem,
+  TrailingActions,
+  SwipeAction,
+} from 'react-swipeable-list';
 import { UserImage, Button } from '@/components/atoms';
 
 import Styled from './NotificationContent.styled';
 
 const NotificationContent = () => {
-  const scrollAreaRef = useRef<HTMLDivElement>(null);
-  const target = useRef<HTMLDivElement>(null);
-
-  const loadItems = () => {
-    setData((prevState) => {
-      const articles = [
-        { id: 11, name: '홍' },
-        { id: 12, name: 'ㅂ' },
-        { id: 13, name: 'ㅈ' },
-        { id: 14, name: 'ㄷ' },
-        { id: 15, name: 'ㄱ' },
-        { id: 16, name: 'ㅅ' },
-        { id: 17, name: 'ㅛ' },
-        { id: 18, name: 'ㅕ' },
-        { id: 19, name: 'ㅑ' },
-        { id: 20, name: 'ㅐ' },
-        { id: 21, name: 'ㅔ' },
-        { id: 22, name: '짱' },
-        { id: 23, name: 'ㄴ' },
-        { id: 24, name: 'ㅇ' },
-        { id: 25, name: 'ㄹ' },
-        { id: 26, name: 'ㅎ' },
-        { id: 27, name: 'ㅗ' },
-        { id: 28, name: 'ㅓ' },
-        { id: 29, name: 'ㅏ' },
-        { id: 30, name: 'ㅣ' },
-        { id: 31, name: 'ㅔ' },
-        { id: 32, name: 'ㅋ' },
-      ];
-      const id = prevState[prevState.length - 1].id;
-      const articleId = articles.map((article, index) => {
-        return { ...article, id: id + index + 1 };
-      });
-      return [...prevState, ...articleId];
-    });
-  };
-
-  useEffect(() => {
-    const options = {
-      root: scrollAreaRef.current,
-      threshold: 0.4,
-    };
-
-    const onIntersect: IntersectionObserverCallback = (entries, observer) => {
-      entries.forEach((entry) => {
-        if (!entry.isIntersecting) {
-          return;
-        }
-        loadItems();
-        observer.unobserve(entry.target);
-        observer.observe(target.current);
-      });
-    };
-
-    const io = new IntersectionObserver(onIntersect, options);
-
-    if (target.current) {
-      io.observe(target.current);
-    }
-
-    return () => io && io.disconnect();
-  }, [target, scrollAreaRef]);
-
   const [datas, setData] = useState([
-    { id: 0, name: '김이박' },
-    { id: 1, name: '김' },
-    { id: 2, name: '이' },
-    { id: 3, name: '박' },
-    { id: 4, name: '황' },
-    { id: 5, name: '정' },
-    { id: 6, name: '한' },
-    { id: 7, name: '길' },
-    { id: 8, name: '선우' },
-    { id: 9, name: '독고' },
+    { id: 0, name: '김' },
+    { id: 1, name: '이' },
+    { id: 2, name: '박' },
+    { id: 3, name: '정' },
+    { id: 4, name: '백' },
+    { id: 5, name: '주' },
   ]);
 
   const follow = () => {
@@ -89,26 +27,19 @@ const NotificationContent = () => {
     setData(datas.filter((value, index) => value.id !== id));
   };
 
-  //grahams.tistory.com/193 [MakeDesire]
+  const trailingActions = (id: number) => (
+    <TrailingActions>
+      <SwipeAction children={''} destructive={true} onClick={() => removeItem(id)} />
+    </TrailingActions>
+  );
 
-  출처: https: return (
-    <Styled.NotificationContainer ref={scrollAreaRef}>
+  return (
+    <Styled.NotificationContainer>
       <SwipeableList>
         {datas.map((notifi, idx) => {
-          const lastEl = idx === datas.length - 1;
           return (
-            <SwipeableListItem
-              key={notifi.id}
-              swipeLeft={{
-                content: (
-                  <div style={{ width: '100%', height: '100%', verticalAlign: 'middle' }}></div>
-                ),
-                action: () => {
-                  removeItem(notifi.id);
-                },
-              }}
-            >
-              <Styled.Notification key={idx} ref={lastEl ? target : null}>
+            <SwipeableListItem key={notifi.id} trailingActions={trailingActions(notifi.id)}>
+              <Styled.Notification key={idx}>
                 <Styled.UserImageContainer>
                   <UserImage userId={1} />
                 </Styled.UserImageContainer>
