8ccf11230ace57957e84e2a07b32250c9b62274f
dohyun <dodohyun0807@gmail.com>
2022-02-08 21:59:15 +0900

    [docs]타입 표기

--- frontend/src/pages/notification/index.tsx
+++ frontend/src/pages/notification/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Styled from './styled';
 import FooterBar from '@/components/FooterNavbar/FooterNavbar';
+import type { NextPage } from 'next';
 
 import Header from '@/components/Header/Header';
 import NotificationContent from '@/components/NotificationContent/NotificationContent';
 
 ///////////////////////////
 
-const Notification = () => {
+const Notification: NextPage = () => {
   return (
     <Styled.NotificationLayout>
       <Header leftSide="pointer" name="알림" />
