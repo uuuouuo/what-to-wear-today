5c85d2da909de737af8fa4970cb8856aafc207f0
dohyun <dodohyun0807@gmail.com>
2022-02-08 21:58:12 +0900

    [docs]타입 설정 및 레이아웃 변경

--- frontend/src/pages/feed/index.tsx
+++ frontend/src/pages/feed/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Styled from './styled';
+import type { NextPage } from 'next';
 
 import FeedRegion from '@/components/FeedRegion/FeedRegion';
 import FeedFilter from '@/components/FeedFilter/FeedFilter';
@@ -7,26 +8,33 @@ import FeedContent from '@/components/FeedContent/FeedContent';
 import FooterNavbar from '@/components/FooterNavbar/FooterNavbar';
 import Header from '@/components/Header/Header';
 
-const Feed = () => {
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+
+const Feed: NextPage = () => {
   return (
-    <Styled.FeedLayout>
-      <Header leftSide="logo" name="오늘 뭐 입지?" rightSide="notification"></Header>
+    <>
+      <Styled.FeedLayout>
+        <Header leftSide="logo" name="오늘 뭐 입지?" rightSide="notification"></Header>
 
-      <Styled.FeedNavbar>
-        <FeedRegion />
-        <div>날씨정보</div>
-      </Styled.FeedNavbar>
+        <Styled.FeedNavbar>
+          <FeedRegion />
+          {/* 날씨 정보창 */}
+          <Styled.WeatherArea>
+            <WbSunnyIcon />
+            -3
+          </Styled.WeatherArea>
+        </Styled.FeedNavbar>
 
-      <Styled.FeedFilter>
-        <FeedFilter />
-      </Styled.FeedFilter>
+        <Styled.FeedFilter>
+          <FeedFilter />
+        </Styled.FeedFilter>
 
-      <Styled.FeedContent>
-        <FeedContent />
-      </Styled.FeedContent>
-
+        <Styled.FeedContent>
+          <FeedContent />
+        </Styled.FeedContent>
+      </Styled.FeedLayout>
       <FooterNavbar />
-    </Styled.FeedLayout>
+    </>
   );
 };
 
--- frontend/src/pages/feed/styled.ts
+++ frontend/src/pages/feed/styled.ts
@@ -4,6 +4,7 @@ const FeedLayout = styled.div`
   display: flex;
   flex-flow: column nowrap;
   height: 100vh;
+
   filter: drop-shadow(0px 20px 10px rgba(0, 0, 0, 0.15));
   background-image: linear-gradient(135deg, #ad6bff 0%, #74bcf7 100%);
 `;
@@ -13,14 +14,13 @@ const FeedNavbar = styled.div`
   flex-flow: row nowrap;
   justify-content: space-between;
   align-items: center;
-  border: 1px solid green;
+  padding-right: 5vw;
 `;
 
 const FeedFilter = styled.div`
   display: flex;
   flex-flow: row nowrap;
   justify-content: center;
-  border: 1px solid purple;
 `;
 
 const FeedContent = styled.div`
@@ -28,10 +28,16 @@ const FeedContent = styled.div`
   flex-flow: column nowrap;
   justify-content: space-evenly;
   align-items: center;
-  border: 1px solid orange;
   word-break: break-all;
   background-color: white;
   overflow: auto;
 `;
 
-export default { FeedLayout, FeedNavbar, FeedFilter, FeedContent };
+const WeatherArea = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export default { FeedLayout, FeedNavbar, FeedFilter, FeedContent, WeatherArea };
