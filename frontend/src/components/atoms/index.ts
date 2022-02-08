93313a28925a725d42d25d320b0004b4a060da3b
16010948 <bjyoung0@gmail.com>
2022-02-08 11:17:53 +0900

    [feat] atom export 추가

--- frontend/src/components/atoms/index.ts
+++ frontend/src/components/atoms/index.ts
@@ -3,10 +3,12 @@ import CheckInput from './CheckInput/CheckInput';
 import FileInput from './FileInput/FileInput';
 import FooterContainer from './FooterContainer/FooterContainer';
 import Heading from './Heading/Heading';
+import Image from './Image/Image';
 import Input from './Input/Input';
 import Label from './Label/Label';
 import MainContainer from './MainContainer/MainContainer';
 import Text from './Text/Text';
+import Textarea from './Textarea/Textarea';
 
 export {
   Button,
@@ -14,8 +16,10 @@ export {
   FileInput,
   FooterContainer,
   Heading,
+  Image,
   Input,
   Label,
   MainContainer,
   Text,
+  Textarea,
 };
