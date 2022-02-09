cb90aaf28ce98f012eda222ee13ec16edba5922c
16010948 <bjyoung0@gmail.com>
2022-02-09 11:57:01 +0900

    [fix] 버그 수정

--- frontend/src/pages/feed/write/styled.ts
+++ frontend/src/pages/feed/write/styled.ts
@@ -1,23 +1,23 @@
-import styled from '@emotion/styled';
-import { TextEditor as BaseTextEditor, ImageList as BaseImageList } from '@/components/molecules';
+// import styled from '@emotion/styled';
+// import { TextEditor as BaseTextEditor, ImageList as BaseImageList } from '@/components/molecules';
 
-const ContentContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  transform: translateY(-5vh);
-  margin: 5vh;
-`;
+// const ContentContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   transform: translateY(-5vh);
+//   margin: 5vh;
+// `;
 
-const ImageList = styled(BaseImageList)`
-  height: 20vh;
-`;
+// const ImageList = styled(BaseImageList)`
+//   height: 20vh;
+// `;
 
-const TextEditor = styled(BaseTextEditor)`
-  width: calc(100% - 4vh);
-  box-sizing: border-box;
-  height: 30vh;
-  margin: 0 2vh;
-`;
+// const TextEditor = styled(BaseTextEditor)`
+//   width: calc(100% - 4vh);
+//   box-sizing: border-box;
+//   height: 30vh;
+//   margin: 0 2vh;
+// `;
 
-export default { ContentContainer, ImageList, TextEditor };
+// export default { ContentContainer, ImageList, TextEditor };
