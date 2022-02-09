790a1b937d6c885fc772b3cc91c36c43dbae3416
16010948 <bjyoung0@gmail.com>
2022-02-09 12:10:56 +0900

    [fix] 버그 픽스

--- frontend/src/pages/login/styled.ts
+++ frontend/src/pages/login/styled.ts
@@ -1,32 +1,32 @@
 import styled from '@emotion/styled';
-import {
-  MainContainer as BaseMainContainer,
-  Heading as Heading,
-  Text as BaseText,
-  FooterContainer as BaseFooterContainer,
-} from '@/components/atoms';
+// import {
+//   MainContainer as BaseMainContainer,
+//   Heading as Heading,
+//   Text as BaseText,
+//   FooterContainer as BaseFooterContainer,
+// } from '@/components/atoms';
 
-import { KakaoButton as BaseKakaoButton } from 'components/molecules';
+// import { KakaoButton as BaseKakaoButton } from 'components/molecules';
 
-const MainContainer = styled(BaseMainContainer)`
-  text-align: center;
-`;
+// const MainContainer = styled(BaseMainContainer)`
+//   text-align: center;
+// `;
 
-const LogoContainer = styled(Heading)`
-  margin-top: 15vh;
-`;
+// const LogoContainer = styled(Heading)`
+//   margin-top: 15vh;
+// `;
 
-const Title = styled(BaseText)`
-  display: block;
-  margin-top: 8vh;
-  letter-spacing: 0.5rem;
-  filter: drop-shadow(0px 50px 25px rgba(0, 0, 0, 0.15));
-`;
+// const Title = styled(BaseText)`
+//   display: block;
+//   margin-top: 8vh;
+//   letter-spacing: 0.5rem;
+//   filter: drop-shadow(0px 50px 25px rgba(0, 0, 0, 0.15));
+// `;
 
-const KakaoButton = styled(BaseKakaoButton)`
-  width: 100%;
-  padding: 0;
-  margin: 5vh;
-`;
+// const KakaoButton = styled(BaseKakaoButton)`
+//   width: 100%;
+//   padding: 0;
+//   margin: 5vh;
+// `;
 
-export default { MainContainer, LogoContainer, Title, KakaoButton };
+// export default { MainContainer, LogoContainer, Title, KakaoButton };
