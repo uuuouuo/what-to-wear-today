bec41f118652ff338d26c28b2f1c77987215721e
강민철 <kmc9308@naver.com>
2022-02-09 14:28:38 +0900

    [docs] 검색 페이지 인풋, 버튼 추가

--- frontend/src/pages/search/index.tsx
+++ frontend/src/pages/search/index.tsx
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
 import Styled from './styled';
 import { Title } from '@/components/molecules';
+import { Button } from 'components/atoms';
+import { useChange } from '@/hooks';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Search = () => {
+  const [value, setValue, onChange] = useChange<HTMLInputElement>('');
+
+  const handleSearch = () => {};
+
   return (
     <Styled.MainContainer>
       <Title value="Search" />
+      <Styled.contentContainer>
+        <Styled.rowContainer>
+          <Styled.Input value={value} onChange={onChange} placeholder="Search..."></Styled.Input>
+          <Button onClick={handleSearch}>
+            <SearchIcon />
+          </Button>
+        </Styled.rowContainer>
+      </Styled.contentContainer>
     </Styled.MainContainer>
   );
 };
--- frontend/src/pages/search/styled.ts
+++ frontend/src/pages/search/styled.ts
@@ -1,9 +1,36 @@
 import styled from '@emotion/styled';
 import { MainContainer as BaseMainContainer } from '@/components/atoms';
+import { Input as BaseInput } from '@/components/atoms';
 
 const MainContainer = styled(BaseMainContainer)`
   text-align: center;
   justify-content: start;
 `;
 
-export default { MainContainer };
+const Input = styled(BaseInput)`
+  width: 75vw;
+`;
+
+const rowContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const columnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const contentContainer = styled.div`
+  margin-top: 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+export default { MainContainer, Input, rowContainer, columnContainer, contentContainer };
