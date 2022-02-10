a31549d5fe13c097725439b5fd5fddbde38dc250
dohyun <dodohyun0807@gmail.com>
2022-02-10 14:29:51 +0900

    [feat]feed 리덕스 작성

--- frontend/src/reducers/index.ts
+++ frontend/src/reducers/index.ts
@@ -1,11 +1,13 @@
 import user from './user';
 import comment from './comment';
+import feed from './feed';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   index: (state = {}, action) => state,
   user,
   comment,
+  feed,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
