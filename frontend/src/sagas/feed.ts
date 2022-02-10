3adf76569779c414b0f027dc4b6d287a18e726c2
dohyun <dodohyun0807@gmail.com>
2022-02-10 14:30:48 +0900

    [feat]feed 관련 saga 함수작성

---
+++ frontend/src/sagas/feed.ts
@@ -0,0 +1,37 @@
+import { all, fork, put, takeLatest, call } from 'redux-saga/effects';
+import { apiInstance } from '@/libs/axios';
+import { AxiosResponse } from 'axios';
+
+import { LOAD_FEED_REQUEST, LOAD_FEED_SUCCESS, LOAD_FEED_FAILURE } from '@/action/feedAction';
+import { FeedType } from '@/types/feed';
+
+const api = apiInstance();
+
+function loadFeedAPI(feedNo: number): Promise<AxiosResponse<FeedType[]>> {
+  console.log(api.get(`/feed/${feedNo}`));
+  return api.get(`/feed/${feedNo}`);
+}
+
+function* loadFeed(action: any) {
+  console.log('zzzz', action);
+  try {
+    const result: Promise<AxiosResponse<FeedType[]>> = yield call(loadFeedAPI, action.feedNo);
+    yield put({
+      type: LOAD_FEED_SUCCESS,
+      data: result,
+    });
+  } catch (err: any) {
+    yield put({
+      type: LOAD_FEED_FAILURE,
+      error: err.response,
+    });
+  }
+}
+
+function* watchLoadFeed() {
+  yield takeLatest(LOAD_FEED_REQUEST, loadFeed);
+}
+
+export default function* feedSaga() {
+  yield all([fork(watchLoadFeed)]);
+}
