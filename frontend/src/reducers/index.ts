bfc8b8b13f045a807ed5937fd8999903e6c18a57
강민철 <kmc9308@naver.com>
2022-02-11 17:02:51 +0900

    [docs] 루트리듀서에 mypageReducer추가

--- frontend/src/reducers/index.ts
+++ frontend/src/reducers/index.ts
@@ -1,11 +1,13 @@
 import user from './user';
 import comment from './comment';
+import mypageFeeds from './mypageFeeds';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   index: (state = {}, action) => state,
   user,
   comment,
+  mypageFeeds,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
