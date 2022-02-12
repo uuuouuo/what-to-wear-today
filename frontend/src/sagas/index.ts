e861391186483e3570564453a5adb1847190d7f9
강민철 <kmc9308@naver.com>
2022-02-13 01:01:12 +0900

    [docs] 루트사가
    - 회원 페이지 코멘트 사가 루트사가에 추가

--- frontend/src/sagas/index.ts
+++ frontend/src/sagas/index.ts
@@ -1,7 +1,8 @@
 import { all, fork } from 'redux-saga/effects';
 import commentSaga from './comment';
 import myFeedsSaga from './mypageFeeds';
+import myCommentsSaga from './mypageComments';
 
 export default function* rootSaga() {
-  yield all([fork(commentSaga), fork(myFeedsSaga)]);
+  yield all([fork(commentSaga), fork(myFeedsSaga), fork(myCommentsSaga)]);
 }
