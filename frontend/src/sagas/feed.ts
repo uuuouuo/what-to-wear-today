175249fd59b1faf6341925a0a2e238869a819941
dohyun <dodohyun0807@gmail.com>
2022-02-13 03:09:34 +0900

    [docs]오류 코드 수정 err.name과 api로 넘어오는 값에 따른 코드 수정

--- frontend/src/sagas/feed.ts
+++ frontend/src/sagas/feed.ts
@@ -1,28 +1,26 @@
 import { all, fork, put, takeLatest, call } from 'redux-saga/effects';
 import { apiInstance } from '@/libs/axios';
 import { AxiosResponse } from 'axios';
-
 import { LOAD_FEED_REQUEST, LOAD_FEED_SUCCESS, LOAD_FEED_FAILURE } from '@/action/feedAction';
 import { FeedType } from '@/types/feed';
 
 const api = apiInstance();
 
-function loadFeedAPI(feedNo: number): Promise<AxiosResponse<FeedType>> {
-  return api.get(`/feed/${feedNo}`);
+function loadFeedAPI(payload: number): Promise<AxiosResponse<FeedType>> {
+  return api.get(`/feed/${payload}`);
 }
 
 function* loadFeed(action: any) {
   try {
-    const result: Promise<AxiosResponse<FeedType>> = yield call(loadFeedAPI, action.feedNo.feedNo);
-    console.log(result);
+    const result: Promise<AxiosResponse<FeedType>> = yield call(loadFeedAPI, action.payload);
     yield put({
       type: LOAD_FEED_SUCCESS,
-      data: result,
+      data: result.data.content,
     });
   } catch (err: any) {
     yield put({
       type: LOAD_FEED_FAILURE,
-      error: err.response,
+      error: err.name,
     });
   }
 }
