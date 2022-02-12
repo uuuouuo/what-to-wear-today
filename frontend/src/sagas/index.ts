52447b04703d634b8d9e9d310b3dc5003f1da238
강민철 <kmc9308@naver.com>
2022-02-12 22:06:40 +0900

    [docs] myFeedsSaga 루트사가에 추가

--- frontend/src/sagas/index.ts
+++ frontend/src/sagas/index.ts
@@ -1,6 +1,7 @@
 import { all, fork } from 'redux-saga/effects';
 import commentSaga from './comment';
+import myFeedsSaga from './mypageFeeds';
 
 export default function* rootSaga() {
-  yield all([fork(commentSaga)]);
+  yield all([fork(commentSaga), fork(myFeedsSaga)]);
 }
