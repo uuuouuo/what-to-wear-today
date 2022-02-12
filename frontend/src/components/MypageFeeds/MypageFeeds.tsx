0d058310b0baccecc243377ed3ec713a6079421a
강민철 <kmc9308@naver.com>
2022-02-12 23:28:38 +0900

    [docs] 피드 작성 목록 컴포넌트 생성

---
+++ frontend/src/components/MypageFeeds/MypageFeeds.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import Styled from './MypageFeeds.styled';
+import type { RootState } from '@/reducers';
+import { useDispatch, useSelector } from 'react-redux';
+import { LOAD_MYPAGE_Feed_REQUEST } from '@/action/MypageFeedAction';
+
+const MypageFeeds = () => {
+  const dispatch = useDispatch();
+  const { myFeeds } = useSelector((state: RootState) => state.mypageFeeds);
+  useEffect(() => {
+    dispatch({
+      type: LOAD_MYPAGE_Feed_REQUEST,
+      userId: 'jdb',
+    });
+  }, []);
+
+  console.log(myFeeds.data);
+
+  return (
+    <Styled.contentContainer>
+      {myFeeds.data !== undefined
+        ? myFeeds.data.map((item, idx) => {
+            return <Styled.columnContainer key={idx}>{item.content}</Styled.columnContainer>;
+          })
+        : null}
+    </Styled.contentContainer>
+  );
+};
+
+export default MypageFeeds;
