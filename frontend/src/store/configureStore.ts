a67fe6d4fbcb27d2d9d5eaac9d04ffffda7e70ae
dohyun <dodohyun0807@gmail.com>
2022-02-13 03:24:07 +0900

    [docs]주석 삭제

--- frontend/src/store/configureStore.ts
+++ frontend/src/store/configureStore.ts
@@ -1,18 +1,3 @@
-// import { createWrapper } from 'next-redux-wrapper';
-// import { createStore, applyMiddleware, Store, Action } from 'redux';
-// import createSagaMiddleware from 'redux-saga';
-
-// import rootSaga from '@/sagas';
-// import rootReducer from '@/reducers';
-
-// export default createWrapper((): Store<any, Action> => {
-//   const sagaMiddleware = createSagaMiddleware();
-//   const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
-//   store.sagaTask = sagaMiddleware.run(rootSaga);
-
-//   return store;
-// });
-
 import { createStore, applyMiddleware, Middleware, StoreEnhancer } from 'redux';
 import rootReducer from '@/reducers';
 import { MakeStore, createWrapper, Context } from 'next-redux-wrapper';
@@ -27,15 +12,15 @@ const configureStore: MakeStore<RootState> = () => {
   const middlewares = [sagaMiddleware];
   const enhancer =
     process.env.NODE_ENV === 'production'
-      ? compose(applyMiddleware(...middlewares)) //배포용 미들웨어
-      : composeWithDevTools(applyMiddleware(...middlewares)); // 개발용 미들웨어
+      ? compose(applyMiddleware(...middlewares))
+      : composeWithDevTools(applyMiddleware(...middlewares));
   const store = createStore(rootReducer, enhancer);
   store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
 const wrapper = createWrapper<RootState>(configureStore, {
-  debug: process.env.NODE_ENV === 'development', // true일 때 디버그가 더 자세히 뜹니다.
+  debug: process.env.NODE_ENV === 'development',
 });
 
 export default wrapper;
