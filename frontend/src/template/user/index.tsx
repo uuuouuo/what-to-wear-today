0245625ad8b79b22b0626e1f9520a29739014d14
강민철 <kmc9308@naver.com>
2022-02-13 17:42:35 +0900

    [docs] 유저 페이지
    - 더미데이터 삭제
    - 미사용 코드 삭제
    - 유저 아이디 백엔드 연동
    - Likes 탭 삭제

--- frontend/src/template/user/index.tsx
+++ frontend/src/template/user/index.tsx
@@ -6,7 +6,6 @@ import { Title } from '@/components/molecules';
 import { Label, Button, Text } from '@/components/atoms';
 import Avatar from '@mui/material/Avatar';
 import SettingsIcon from '@mui/icons-material/Settings';
-import SearchIcon from '@mui/icons-material/Search';
 
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
@@ -29,21 +28,6 @@ const User: NextPage<Props> = ({ userId }) => {
     setValue(newValue);
   };
 
-  const dummyLike = [
-    { title: '더미 라이크 1', content: '헤헿' },
-    { title: '더미 라이크 2', content: '헤헿' },
-    { title: '더미 라이크 3', content: '헤헿' },
-    { title: '더미 라이크 4', content: '헤헿' },
-    { title: '더미 라이크 5', content: '헤헿' },
-  ];
-  const dummyComment = [
-    { title: '더미 코멘트 1', content: '헤헿' },
-    { title: '더미 코멘트 2', content: '헤헿' },
-    { title: '더미 코멘트 3', content: '헤헿' },
-    { title: '더미 코멘트 4', content: '헤헿' },
-    { title: '더미 코멘트 5', content: '헤헿' },
-  ];
-
   const modifyProfile = () => {
     console.log(userId);
     console.log(typeof userId);
@@ -57,7 +41,7 @@ const User: NextPage<Props> = ({ userId }) => {
         <Avatar src={'/images/icon/blank_user.png'} sx={{ width: 100, height: 100 }} />
         <Styled.columnContainer>
           <Styled.rowContainer>
-            <Label>보노보노</Label>
+            <Label>{userId}</Label>
             {Number(userId) === 1 ? (
               <Button onClick={modifyProfile}>
                 <SettingsIcon />
@@ -81,7 +65,6 @@ const User: NextPage<Props> = ({ userId }) => {
         <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
           <Tabs value={value} onChange={handleChange} centered>
             <Tab label="Feeds" />
-            <Tab label="Likes" />
             <Tab label="Comments" />
           </Tabs>
         </Box>
@@ -89,7 +72,7 @@ const User: NextPage<Props> = ({ userId }) => {
       <Styled.contentContainer></Styled.contentContainer>
       <Styled.contentContainer>
         {value === 0 && userId !== undefined ? <MypageFeeds userId={userId} /> : null}
-        {value === 2 && userId !== undefined ? <MypageComments userId={userId} /> : null}
+        {value === 1 && userId !== undefined ? <MypageComments userId={userId} /> : null}
       </Styled.contentContainer>
     </Styled.MainContainer>
   );
