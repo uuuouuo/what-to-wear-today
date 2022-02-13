67de6703bcabc74e71028aaba96baac65bce9442
강민철 <kmc9308@naver.com>
2022-02-13 16:49:39 +0900

    [docs] 유저페이지 서버사이드 props

--- frontend/src/pages/user/[userId].tsx
+++ frontend/src/pages/user/[userId].tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import { NextPage } from 'next';
 import UserTemplate from '@/template/user';
+import wrapper from '@/store/configureStore';
+import { END } from 'redux-saga';
+import { loadMypageFeedsRequest } from 'action/MypageFeedAction';
 
-const User: NextPage = () => {
-  return <UserTemplate></UserTemplate>;
+interface Props {
+  userId: string;
+}
+
+const User: NextPage<Props> = ({ userId }) => {
+  return <UserTemplate userId={userId}></UserTemplate>;
 };
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) =>
+    async ({ req, res, params }) => {
+      const data = params.userId;
+      store.dispatch(loadMypageFeedsRequest(data));
+      store.dispatch(END);
+      await store.sagaTask.toPromise();
+      return { props: { userId: data } };
+    },
+);
 
 export default User;
