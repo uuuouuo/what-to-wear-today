d1fb73f92a71eafc3297e61cff26a78c718a274e
강민철 <kmc9308@naver.com>
2022-02-13 16:50:39 +0900

    [docs] 마이 피드 컴포넌트
    - serverside props 적용

--- frontend/src/components/MypageFeeds/MypageFeeds.tsx
+++ frontend/src/components/MypageFeeds/MypageFeeds.tsx
@@ -4,25 +4,14 @@ import type { RootState } from '@/reducers';
 import { useDispatch, useSelector } from 'react-redux';
 import { LOAD_MYPAGE_Feed_REQUEST } from '@/action/MypageFeedAction';
 
-const MypageFeeds = ({ userId }) => {
-  const dispatch = useDispatch();
+const MypageFeeds = () => {
   const { myFeeds } = useSelector((state: RootState) => state.mypageFeeds);
-  useEffect(() => {
-    dispatch({
-      type: LOAD_MYPAGE_Feed_REQUEST,
-      userId: userId,
-    });
-  }, []);
-
-  // console.log(myFeeds.data);
 
   return (
     <Styled.contentContainer>
-      {myFeeds.data !== undefined
-        ? myFeeds.data.map((item, idx) => {
-            return <Styled.columnContainer key={idx}>{item.content}</Styled.columnContainer>;
-          })
-        : null}
+      {myFeeds.map((item, idx) => {
+        return <Styled.columnContainer key={idx}>{item.content}</Styled.columnContainer>;
+      })}
     </Styled.contentContainer>
   );
 };
