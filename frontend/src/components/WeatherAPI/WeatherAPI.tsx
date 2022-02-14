4b0ba12ea3488420da6a729ec697416e88921f13
강민철 <kmc9308@naver.com>
2022-02-14 22:34:52 +0900

    [feat] 날씨 API

---
+++ frontend/src/components/WeatherAPI/WeatherAPI.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect, FunctionComponent } from 'react';
+import { apiInstance } from '@/libs/axios';
+
+interface Props {
+  region: string;
+  date: string;
+}
+
+const WeatherAPI: FunctionComponent<Props> = ({ region, date }) => {
+  const [weatherData, setWeatherData] = useState();
+  const api = apiInstance();
+  useEffect(() => {
+    const weather: Promise<any> = api
+      .get(`/weather?region=${region}&date=${date}`)
+      .then((res) => setWeatherData(res));
+  }, []);
+
+  return (
+    <div>
+      {weatherData ? weatherData.data.temperatures : 'Loading'}
+      {weatherData ? weatherData.data.curWeather : 'Loading'}
+    </div>
+  );
+};
+
+export default WeatherAPI;
