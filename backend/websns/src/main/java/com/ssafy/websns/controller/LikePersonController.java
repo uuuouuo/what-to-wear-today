3ff06c63c959bc4bc9e2cf38a62e9ff52671755a
jdb4497 <jdb4497@nate.com>
2022-02-14 13:20:19 +0900

    [fix] bug fix

--- backend/websns/src/main/java/com/ssafy/websns/controller/LikePersonController.java
+++ backend/websns/src/main/java/com/ssafy/websns/controller/LikePersonController.java
@@ -1,43 +1,18 @@
 package com.ssafy.websns.controller;
 
-import com.ssafy.websns.model.dto.feed.LikePersonDto.LikePersonRes;
-import com.ssafy.websns.service.feed.LikePersonService;
+import com.ssafy.websns.model.dto.feed.FeedDto.FeedRes;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/like")
 @RequiredArgsConstructor
-public class LikePersonController {
+public class LIkePersonController {
 
-  private final LikePersonService likePersonService;
-
-  @PostMapping(value = "/{userId}/{feedNo}")
-  public ResponseEntity<LikePersonRes> createLike(@RequestBody String userId, @RequestBody Integer feedNo) {
-
-    LikePersonRes response = likePersonService.createLike(userId, feedNo);
-    return new ResponseEntity<>(response, HttpStatus.OK);
+  public ResponseEntity<> createLike() {
 
   }
-
-  @DeleteMapping(value = "/{likeNo}")
-  public void deleteLike(@PathVariable("likeNo") Integer likeNo) {
-
-    likePersonService.cancelLike(likeNo);
-
-  }
-
-//  @GetMapping(value = "/{feedNo}")
-//  public List<String> findLikePeople(@PathVariable("feedNo") Integer feedNo) {
-//
-//    likePersonService.showLikePeople(feedNo);
-//  }
 
 }
