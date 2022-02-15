623bc08a6a084452f9cfaeafd060d4128374e5fa
장소명 <somyeong3@naver.com>
2022-02-15 16:24:47 +0900

    [fix] fix bug

--- backend/websns/src/main/java/com/ssafy/websns/repository/region/RegionRepository.java
+++ backend/websns/src/main/java/com/ssafy/websns/repository/region/RegionRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.websns.repository.region;
 
 import com.ssafy.websns.model.entity.region.Region;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RegionRepository extends JpaRepository<Region,Integer> {
