76a48808a2a7f1755e53623cc52bb26ff16a650c
16010948 <bjyoung0@gmail.com>
2022-02-15 16:33:31 +0900

    [refactor] 파일 대소문자 변경

--- frontend/src/action/myPageCommentAction.ts
+++ frontend/src/action/myPageCommentAction.ts
@@ -2,15 +2,15 @@ export const LOAD_MYPAGE_COMMENT_REQUEST = 'LOAD_MYPAGE_COMMENT_REQUEST';
 export const LOAD_MYPAGE_COMMENT_SUCCESS = 'LOAD_MYPAGE_COMMENT_SUCCESS';
 export const LOAD_MYPAGE_COMMENT_FAILURE = 'LOAD_MYPAGE_COMMENT_FAILURE';
 
-export const loadMypageCOMMENTsRequest = (userId: string) => ({
+export const loadMypageCommentsRequest = (userId: string) => ({
   type: LOAD_MYPAGE_COMMENT_REQUEST,
-  userId: userId,
+  userId,
 });
 
-export const loadMypageCOMMENTsSuccess = () => ({
+export const loadMypageCommentsSuccess = () => ({
   type: LOAD_MYPAGE_COMMENT_SUCCESS,
 });
 
-export const loadMypageCOMMENTsFailure = () => ({
+export const loadMypageCommentsFailure = () => ({
   type: LOAD_MYPAGE_COMMENT_FAILURE,
 });
--- frontend/src/action/myPageFeedAction.ts
+++ frontend/src/action/myPageFeedAction.ts
@@ -1,16 +1,16 @@
-export const LOAD_MYPAGE_Feed_REQUEST = 'LOAD_MYPAGE_Feed_REQUEST';
-export const LOAD_MYPAGE_Feed_SUCCESS = 'LOAD_MYPAGE_Feed_SUCCESS';
-export const LOAD_MYPAGE_Feed_FAILURE = 'LOAD_MYPAGE_Feed_FAILURE';
+export const LOAD_MYPAGE_FEED_REQUEST = 'LOAD_MYPAGE_Feed_REQUEST';
+export const LOAD_MYPAGE_FEED_SUCCESS = 'LOAD_MYPAGE_Feed_SUCCESS';
+export const LOAD_MYPAGE_FEED_FAILURE = 'LOAD_MYPAGE_Feed_FAILURE';
 
 export const loadMypageFeedsRequest = (userId: string) => ({
-  type: LOAD_MYPAGE_Feed_REQUEST,
-  userId: userId,
+  type: LOAD_MYPAGE_FEED_REQUEST,
+  userId,
 });
 
 export const loadMypageFeedsSuccess = () => ({
-  type: LOAD_MYPAGE_Feed_SUCCESS,
+  type: LOAD_MYPAGE_FEED_SUCCESS,
 });
 
 export const loadMypageFeedsFailure = () => ({
-  type: LOAD_MYPAGE_Feed_FAILURE,
+  type: LOAD_MYPAGE_FEED_FAILURE,
 });
