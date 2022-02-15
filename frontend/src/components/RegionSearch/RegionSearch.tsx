a43ba7826ce35436faa71cd34fc1d75f4a0cbf53
강민철 <kmc9308@naver.com>
2022-02-15 11:50:58 +0900

    [docs] 지역설정 컴포넌트 세부작성
    - 추가 및 삭제 기능
    - 백엔드와 연동 필요

--- frontend/src/components/RegionSearch/RegionSearch.tsx
+++ frontend/src/components/RegionSearch/RegionSearch.tsx
@@ -4,22 +4,54 @@ import { apiInstance } from '@/libs/axios';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+
 const RegionSearch: FunctionComponent = () => {
-  const [regionList, setRegionList] = useState();
   const api = apiInstance();
+  const [regionList, setRegionList] = useState();
+  const [interestRegion, setInterestRegion] = useState([]);
+
   useEffect(() => {
     const region: Promise<any> = api.get(`/region`).then((res) => setRegionList(res.data));
   }, []);
 
+  const onChange = (event, value) => {
+    if (value) {
+      setInterestRegion([...interestRegion, value]);
+    }
+  };
+
+  const handleDelete = (event: React.MouseEvent, idx: number) => {
+    const newRegionList = interestRegion.filter((el, i) => {
+      return idx !== i;
+    });
+    setInterestRegion(newRegionList);
+  };
+
   return (
-    <Autocomplete
-      onChange={(event, value) => console.log(value)}
-      disablePortal
-      id="combo-box-demo"
-      options={regionList}
-      sx={{ width: 300 }}
-      renderInput={(params) => <TextField {...params} label="지역" />}
-    />
+    <>
+      <Autocomplete
+        onChange={onChange}
+        disablePortal
+        id="combo-box-demo"
+        options={regionList}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="지역" />}
+      />
+      <Stack direction="column" spacing={1}>
+        {interestRegion.map((region, idx) => {
+          return (
+            <Chip
+              key={idx}
+              label={region}
+              variant="outlined"
+              onDelete={(e: React.MouseEvent) => handleDelete(e, idx)}
+            />
+          );
+        })}
+      </Stack>
+    </>
   );
 };
 
