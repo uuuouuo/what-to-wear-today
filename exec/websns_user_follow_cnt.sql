fae084c7f97739ebcc10ba70c721b24c3d9130fd
장다빈 <jdb4497@nate.com>
2022-02-18 00:28:52 +0000

    Upload New File

---
+++ exec/websns_user_follow_cnt.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: i6c106.p.ssafy.io    Database: websns
+-- ------------------------------------------------------
+-- Server version	8.0.28-0ubuntu0.20.04.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_follow_cnt`
+--
+
+DROP TABLE IF EXISTS `user_follow_cnt`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_follow_cnt` (
+  `follow_cnt_no` int NOT NULL AUTO_INCREMENT,
+  `follower_cnt` int NOT NULL,
+  `following_cnt` int NOT NULL,
+  `user_no` varchar(19) NOT NULL,
+  PRIMARY KEY (`follow_cnt_no`),
+  KEY `FK9cdim4a0hkwhqpw6ldygcbqap` (`user_no`),
+  CONSTRAINT `FK9cdim4a0hkwhqpw6ldygcbqap` FOREIGN KEY (`user_no`) REFERENCES `user` (`user_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_follow_cnt`
+--
+
+LOCK TABLES `user_follow_cnt` WRITE;
+/*!40000 ALTER TABLE `user_follow_cnt` DISABLE KEYS */;
+INSERT INTO `user_follow_cnt` VALUES (1,76,123,'a123412341234'),(2,56,24,'a432143214321'),(3,34,234,'1'),(4,156,34,'2'),(5,23,76,'3'),(6,75,54,'4');
+/*!40000 ALTER TABLE `user_follow_cnt` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-18  8:57:38
